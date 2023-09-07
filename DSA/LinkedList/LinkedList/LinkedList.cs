73b2581261d997fbc1111665866fea6ef580e17c
AlejandroGonzalezDe <alejandro.gonzalezde@outlook.com>
2023-09-07 16:51:33 -0400

    Updated Linked List Tests

--- DSA/LinkedList/LinkedList/LinkedList.cs
+++ DSA/LinkedList/LinkedList/LinkedList.cs
@@ -1,26 +1,33 @@
-﻿namespace LinkedList
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
 {
-    public interface ILinkedLIst<T>
+    public interface ILinkedList<T>
     {
         int Count { get; }
         void AddFirst(INode<T> value);
+        void InsertAfterNodeIndex(INode<T> value, int position);
         void AddLast(INode<T> value);
-        void InsertAfterNodeIndex(INode<T> node, int index);
         void Clear();
-        void RemoveLast();
-        void RemoveAt(int indexPosition);
         void RemoveFirst();
-        void Sort();
-        INode<T> FindFirst(T value);
+        void RemoveAt(int IndexPosition);
+        void RemoveLast();
+        INode<T>? FindFirst(T value);
         INode<T>[] FindAll(T value);
-        INode<T> Head { get; }
-        INode<T> Tail { get; }
-        IEnumerable<INode<T>> Node { get; }
+        INode<T>? Head { get; }
+        INode<T>? Tail { get; }
+
+        IEnumerable<INode<T>> Nodes { get; }
     }
     public interface INode<T>
     {
-        T Content { get; set; } 
+        T Content { get; set; }
         INode<T>? Next();
+        void LinkNext(INode<T> nextNode);
     }
-    
+
 }
