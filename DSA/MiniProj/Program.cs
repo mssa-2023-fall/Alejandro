8464ef5bf834bb5c62f712b78f05baf513740e6a
AlejandroGonzalezDe <alejandro.gonzalezde@outlook.com>
2023-09-16 19:16:54 -0400

    Cleared unsed code

--- DSA/MiniProj/Program.cs
+++ DSA/MiniProj/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BinaryTreeLib;
 using MiniProject;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 
 Console.WriteLine("Hello, World!");
 
@@ -13,6 +16,36 @@ static int[] DivideArray(int[] input, int left, int right)
     return MergeTwoArraysAndSort( DivideArray(input, left, right), DivideArray(input, mid+1, right));
 
 }
+
+//binary search
+
+int[] list3 = { 1,2,3,4,5,6,7,8,9,10,11,12,13 };
+
+int startindex = 0;
+int endindex = list3.Length - 1;
+int midindex = (startindex+endindex) / (2);
+
+TreeNode A = new TreeNode(list3[midindex]);
+
+int midleft = (startindex + (midindex-1)) / 2;
+int midright = (midindex + 1 + endindex) / 2;
+
+     
+
+
+
+	
+	    
+    
+                   int[] array = { 3, 6, 8, 23, 48, 76, 89 };
+                   TreeNode treeRoot = createBST(array);
+                   
+               	    
+	     
+
+
+
+
 
 
 
--- DSA/MiniProject/Node.cs
+++ DSA/MiniProject/Node.cs
@@ -7,14 +7,14 @@ namespace MiniProject
         public TreeNode? Parent;
         public TreeNode? LeftChild;
         public TreeNode? RightChild;
-        public bool IsLeaf => RightChild==null && LeftChild == null;
-        public bool IsRoot => Parent== null;
+        public bool IsLeaf => RightChild == null && LeftChild == null;
+        public bool IsRoot => Parent == null;
         public TreeNode(int value)
         {
             Value = value;
         }
-        
-        
+
+
         public TreeNode(int _value, TreeNode _parent, int _level)
         {
             this.Parent = _parent;
@@ -23,5 +23,30 @@ namespace MiniProject
         }
         public int Value { get; }
         public int Level { get; set; }
+
+
+        public TreeNode createBST(int[] array)
+        {
+            Array.Sort(array);
+            return BalanceTree(array, 0, array.Length - 1);
+        }
+
+        public TreeNode BalanceTree(int[] array, int start, int end)
+        {
+
+            if (array == null || array.Length == 0 || start > end)
+            {
+                return null;
+            }
+
+            int mid = (start + end) / 2;
+            TreeNode root = new TreeNode(array[mid]);
+
+            root.LeftChild = (BalanceTree(array, start, mid - 1));
+            root.RightChild = (BalanceTree(array, mid + 1, end));
+
+            return root;
+        }
+
     }
 }
